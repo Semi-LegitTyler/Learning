bd4780dc6bba58f33aaed36a22e842c95cba9264
Tyler Lang <semilegitstudios@gmail.com>
2017-04-06 13:44:36 -0400

    My first commit

--- Learning/ViewController.swift
+++ Learning/ViewController.swift
@@ -10,6 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstTitle: UILabel!
+    
+    var tapCount = 0
+    
+    @IBAction func buttonTap(_ sender: UIButton) {
+        tapCount = tapCount + 1
+        
+        print(tapCount)
+        
+        if tapCount >= 10 {
+            print ("Slow Down On My Button!")
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
